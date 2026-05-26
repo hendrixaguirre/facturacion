2e7ea3b820023810f3f8ee5da877ed394df00040
HDAZ <hendrixag@uamv.edu.ni>
2026-05-26 11:03:21 -0600

    Creación de clase Dirección y referencias en Cliente

--- src/main/java/com/tuempresa/facturacion/modelo/Cliente.java
+++ src/main/java/com/tuempresa/facturacion/modelo/Cliente.java
@@ -2,9 +2,11 @@ package com.tuempresa.facturacion.modelo;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.openxava.annotations.NoFrame;
 import org.openxava.annotations.Required;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -18,5 +20,8 @@ public class Cliente {
     @Column(length = 50)
     @Required
     String nombre;
+
+    @Embedded @NoFrame// Así para referenciar a una clase incrustable
+    Direccion direccion; // Una referencia Java convencional
 
 }
