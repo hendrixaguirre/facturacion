f5faf494e8cb9e4ec39d6dd35e605c129c2d6844
HDAZ <hendrixag@uamv.edu.ni>
2026-05-26 11:59:30 -0600

    Creación de clase Factura, Detalle, paquete calculadoras y su clase CalculadorSiguienteNumeroParaAnyo

--- src/main/java/com/tuempresa/facturacion/modelo/Cliente.java
+++ src/main/java/com/tuempresa/facturacion/modelo/Cliente.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.openxava.annotations.NoFrame;
 import org.openxava.annotations.Required;
+import org.openxava.annotations.View;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
@@ -12,6 +13,9 @@ import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
+@View(name="Simple", // Esta vista solo se usará cuando se especifique ?Simple?
+        members="numero, nombre" // Muestra únicamente numero y nombre en la misma línea
+)
 public class Cliente {
     @Id
     @Column(length = 8)
